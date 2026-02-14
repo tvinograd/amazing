c3401ef29052e6c31602eed687147faade426f87
Tatiana Vinogradova <tatiana.vinogradova@tum.de>
2026-02-14 16:36:56 +0100

    Moved config parsing into MazeGen, whose init now gets all the variables and we don't pass them around manually anymore

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -57,32 +57,19 @@
 # Choise? (1-4): 
 
 
-from models.config_parser import ConfigParser
 from models.maze_generator import MazeGenerator
 import sys
 
 
 if __name__ == "__main__":
-    config = ConfigParser().parse_config("config.txt")
-    if not config:
-        sys.exit(1)
-    
-    width = config["WIDTH"]
-    height = config["HEIGHT"]
-    entry = config["ENTRY"]
-    exit = config["EXIT"]
-    seed = config["SEED"]
-    perfect = config["PERFECT"]
-
-    maze_generator = MazeGenerator(seed)
+    maze_generator = MazeGenerator("config.txt")
     try:
-        maze_generator.set_canvas(width, height, entry, exit)
+        maze_generator.set_canvas()
     except ValueError as e:
         print(e)
         sys.exit(1)
     maze_generator.set_renderer()
-    maze_generator.generate_maze() # perfect
-    # maze_generator.generate_maze(False) # imperfect
+    maze_generator.generate_maze()
     maze_generator.solve_maze()
 
     while True:
@@ -95,11 +82,13 @@ if __name__ == "__main__":
         choice = input("Choice? (1-4): ")
 
         if choice == "1":
-            maze_generator.regenerate_maze() # perfect
-            # maze_generator.regenerate_maze(False) # imperfect
+            maze_generator.regenerate_maze()
             maze_generator.solve_maze()
+            maze_generator.renderer.path_animated = False
         elif choice == "2":
             maze_generator.renderer.show_path = not maze_generator.renderer.show_path
+            if not maze_generator.renderer.show_path:
+                maze_generator.renderer.path_animated = False
         elif choice == "3":
             maze_generator.renderer.color_index = (maze_generator.renderer.color_index + 1) % len(maze_generator.renderer.wall_colors)
         elif choice == "4":
