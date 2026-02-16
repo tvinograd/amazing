c8b6cda84f8bfa19beed85a2ee59b2b0ee278d15
Irek Iunusov <iiunusov@3-B-9.42heilbronn.de>
2026-02-16 14:46:58 +0100

    finding forbidden blocks fixed

--- models/maze_generator.py
+++ models/maze_generator.py
@@ -45,6 +45,7 @@ class MazeGenerator():
             if not self.perfect:
                 self.remove_dend_walls()
             while self.has_forbidden_opened_block():
+                self.set_canvas()
                 dfs.generate_maze(self.canvas, self.canvas.cells[0], self.rng)
                 if not self.perfect:
                     self.remove_dend_walls()
@@ -124,11 +125,21 @@ class MazeGenerator():
                     queue.append((neighbour, path + [neighbour]))
 
     def has_forbidden_opened_block(self) -> bool:
-        opened = {cell.coordinate for cell in self.canvas.cells if cell.direction.value == Direction.OPENED.value}
-        for x, y in opened:
-            block = {(x + dx, y + dy) for dx in range(3) for dy in range(3)}
-            if block.issubset(opened):
-                return True
+        opened_cells = {cell for cell in self.canvas.cells if cell.direction.value == Direction.OPENED.value}
+        for opened_cell in opened_cells:
+            x, y = opened_cell.coordinate
+            north_neighbour = self.canvas.get_cell(x, y - 1)
+            south_neighbour = self.canvas.get_cell(x, y + 1)
+            west_neighbour = self.canvas.get_cell(x - 1, y)
+            east_neighbour = self.canvas.get_cell(x + 1, y)
+            if not north_neighbour or not south_neighbour or not west_neighbour or not east_neighbour:
+                continue
+            if (not (north_neighbour.direction.can_see(Direction.S) and north_neighbour.direction.can_see(Direction.W) and north_neighbour.direction.can_see(Direction.E)) or
+                not (south_neighbour.direction.can_see(Direction.N) and south_neighbour.direction.can_see(Direction.W) and south_neighbour.direction.can_see(Direction.E)) or
+                not (west_neighbour.direction.can_see(Direction.S) and west_neighbour.direction.can_see(Direction.E) and west_neighbour.direction.can_see(Direction.N)) or
+                not (east_neighbour.direction.can_see(Direction.S) and east_neighbour.direction.can_see(Direction.W) and east_neighbour.direction.can_see(Direction.N))):
+                    continue
+            return True
         return False
 
 
